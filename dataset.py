10ebe9be21e529c30ece953611d934fa933afc0d
Antioch Sanders <antiochsanders@gmail.com>
2021-01-14 10:21:26 -0500

    Add early stop, reorganizations

---
+++ dataset.py
@@ -0,0 +1,39 @@
+from torch.utils.data import Dataset, DataLoader
+from math import inf
+import torch
+import numpy as np
+
+class PuzzleDataset(Dataset):
+    def __init__(self, puzzles_file, ratings_file, length, truncation = inf):
+        self.puzzles = np.memmap(puzzles_file, mode="r", dtype=np.int8, shape=(length, 544, 8, 8))
+        self.ratings = np.memmap(ratings_file, mode="r", dtype=np.float32, shape=(length, 2))
+        self.truncation = truncation
+
+    def __len__(self):
+        return min(len(self.puzzles), self.truncation)
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.puzzles[idx].copy(), dtype=torch.float32),\
+               torch.tensor(self.ratings[idx].copy(), dtype=torch.float32)
+
+def get_datasets(truncation = inf):
+    train_dataset = PuzzleDataset("./data/puzzles_train.dat", "./data/ratings_train.dat",
+                                  558312, truncation = truncation)
+    val_dataset = PuzzleDataset("./data/puzzles_val.dat", "./data/ratings_val.dat",
+                                159517, truncation = truncation)
+    test_dataset = PuzzleDataset("./data/puzzles_test.dat", "./data/ratings_test.dat",
+                                 79760, truncation = truncation)
+
+    return train_dataset, val_dataset, test_dataset
+
+
+def get_dataloaders(truncation = inf):
+    train_dataset, val_dataset, test_dataset = get_datasets(truncation = truncation)
+    print(f"Loaded datasets: {len(train_dataset)} train, "
+           f"{len(val_dataset)} val, {len(test_dataset)} test")
+
+    train_dataloader = DataLoader(train_dataset, batch_size = 64, shuffle = True)
+    val_dataloader = DataLoader(val_dataset, batch_size = 64, shuffle = False)
+    test_dataloader = DataLoader(test_dataset, batch_size = 64, shuffle = False)
+
+    return train_dataloader, val_dataloader, test_dataloader
--- train_predictor.py
+++ train_predictor.py
@@ -1,52 +1,70 @@
 #!/usr/bin/env python3
 
-# imports
+import argparse
+
+
+from math import inf
+import tqdm
 import numpy as np
 import torch
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
 from models import *
+from dataset import get_dataloaders
 
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 TRUNCATION = 3
+MAX_EPOCHS = 45
+EARLY_STOP = 5
 
-class PuzzleDataset(Dataset):
-    def __init__(self, puzzles_file, ratings_file, length):
-        self.puzzles = np.memmap(puzzles_file, mode="r", dtype=np.int8, shape=(length, 544, 8, 8))
-        self.ratings = np.memmap(ratings_file, mode="r", dtype=np.float32, shape=(length, 2))
 
-    def __len__(self):
-        return min(len(self.puzzles), TRUNCATION)
-
-    def __getitem__(self, idx):
-        return torch.tensor(self.puzzles[idx].copy(), dtype=torch.float32),\
-               torch.tensor(self.ratings[idx].copy(), dtype=torch.float32)
 
 
 def main():
-    train_dataset = PuzzleDataset("./data/puzzles_train.dat", "./data/ratings_train.dat", 558312)
-    val_dataset = PuzzleDataset("./data/puzzles_val.dat", "./data/ratings_val.dat", 159517)
-    test_dataset = PuzzleDataset("./data/puzzles_test.dat", "./data/ratings_test.dat", 79760)
-
-    print(f"Loaded datasets: {len(train_dataset)} train, "
-           f"{len(val_dataset)} val, {len(test_dataset)} test")
-    train_dataloader = DataLoader(train_dataset, batch_size = 64, shuffle = True)
-    val_dataloader = DataLoader(val_dataset, batch_size = 64, shuffle = True)
-    model = BasicChessCNN()
+    train_dataloader, val_dataloader, _ = get_dataloaders(truncation = TRUNCATION)
+    model = BasicChessCNN().to(DEVICE)
     optimizer = optim.AdamW(model.parameters(), lr = 1e-3)
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience = 5, factor = 0.3)
 
-    EPOCHS = 45
-    for i in range(EPOCHS):
-        train_loss = train(model, train_dataloader, optimizer, scheduler)
-        val_loss = evaluate(model, val_dataloader)
-        print(f"TRAIN LOSS {train_loss:2f}, VAL LOSS {val_loss:2f}\n")
+    best_val_loss = inf
+    counter = 0
+    for i in range(MAX_EPOCHS):
+        train_loss, train_correct = train(model, train_dataloader, optimizer, scheduler)
+        val_loss, val_correct = evaluate(model, val_dataloader)
+        print(f"EPOCH {i + 1} finished")
+        print(f"TRAIN LOSS {train_loss:.3f}, VAL LOSS {val_loss:.3f}")
+        print(f"TRAIN CORRECT {train_correct:.3f}, VAL CORRECT {val_correct:.3f}")
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            counter = 0
+            save_model(model, optimizer, scheduler, val_loss, i + 1)
+        else:
+            counter += 1
+            print("No improvement")
+            if counter == EARLY_STOP:
+                print("Stopping early")
+                break
+
+        print()
 
     print("finished training model")
 
 
-def scaled_l2_loss(actual, desired):
+def save_model(model, optimizer, scheduler, val_loss, epoch):
+    path = f"models/{type(model).__name__}.pt"
+    print(f"Saving model as {path}")
+    torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                'val_loss': val_loss
+    }, path)
+
+
+def scaled_l2_loss(actual, desired, reduction = 'mean'):
     """
         actual  (batch_size, 1): each value is a predicated value
         desired (batch_size, 2): each row is a value and then uncertainty of that value
@@ -62,40 +80,49 @@ def scaled_l2_loss(actual, desired):
     assert scaled_actual.shape == (batch, 1), scaled_actual.shape
 
     scaled_desired = desired[:, 0, None] / std_dev
-    assert scaled_desired.shape == (3, 1), scaled_desired.shape
-    print("sa", scaled_actual)
-    print("sd", scaled_desired)
+    assert scaled_desired.shape == (batch, 1), scaled_desired.shape
 
-    return F.mse_loss(scaled_actual, scaled_desired)
+    return F.mse_loss(scaled_actual, scaled_desired, reduction = reduction)
 
 
 def train(model, train_dataloader, optimizer, scheduler):
     losses = []
-    for (x, y) in train_dataloader:
+    correct = 0
+    total = 0
+    for (x, y) in tqdm.tqdm(train_dataloader):
         optimizer.zero_grad()
 
-        print(y)
+        x, y = x.to(DEVICE), y.to(DEVICE)
         out = model(x)
-        print(out)
-        loss = scaled_l2_loss(out, y)
-        losses.append(loss.item())
-        loss.backward()
+
+        loss = scaled_l2_loss(out, y, reduction = 'none')
+        loss_reduced = loss.mean()
+        correct += (loss < 1).sum()
+        total += len(x)
+        losses.append(loss_reduced.item())
+
+        loss_reduced.backward()
+
         torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
         optimizer.step()
-        scheduler.step(loss)
+        scheduler.step(loss_reduced)
 
-    return np.mean(losses)
+    return np.mean(losses), correct / total
 
 
 def evaluate(model, eval_dataloader):
+    correct = 0
+    total = 0
     with torch.no_grad():
         losses = []
-        for (x, y) in eval_dataloader:
+        for (x, y) in tqdm.tqdm(eval_dataloader):
             out = model(x)
-            loss = scaled_l2_loss(out, y)
-            losses.append(loss.item())
+            loss = scaled_l2_loss(out, y, reduction = 'none')
+            correct += (loss < 1).sum()
+            total += len(x)
+            losses.append(loss.mean().item())
 
-    return np.mean(losses)
+    return np.mean(losses), correct / total
 
 if __name__ == "__main__":
     main()
