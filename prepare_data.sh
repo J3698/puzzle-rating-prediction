a6e1ba4cd46f0fc697957e5a3b53545cc556bae7
Anti <antiochsanders@gmail.com>
2021-01-26 21:19:37 -0500

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-
+# error handling
 set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
-
+# delegate to other scripts
 ./data_util/ensure_data_downloaded.sh
 ./data_util/data_to_np.py
 ./data_util/split_data.py
